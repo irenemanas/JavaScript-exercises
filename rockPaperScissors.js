b269f3c39b7d9f78c6e169bc2659ddec6a24253e
irenemanas <irenemanas@gmail.com>
2022-01-14 11:10:38 +0100

    Add speed debt calculator

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -63,3 +63,53 @@ const playGame = () => {
 };
 
 playGame();
+
+///SLEEP DEBT CALCULATOR
+
+const getSleepHours = (day) => {
+  if (day === "Monday") {
+    return 8;
+  } else if (day === "Tuesday") {
+    return 8;
+  } else if (day === "Wednesday") {
+    return 8;
+  } else if (day === "Thursday") {
+    return 8;
+  } else if (day === "Friday") {
+    return 8;
+  } else if (day === "Saturday") {
+    return 8;
+  } else if (day === "Sunday") {
+    return 8;
+  }
+};
+
+console.log(getSleepHours("Saturday"));
+
+const getActualSleepHours = () =>
+  getSleepHours("Monday") +
+  getSleepHours("Tuesday") +
+  getSleepHours("Wednesday") +
+  getSleepHours("Thursday") +
+  getSleepHours("Friday") +
+  getSleepHours("Saturday") +
+  getSleepHours("Sunday");
+
+const getIdealSleepHours = () => {
+  const idealHours = 6.5;
+  return idealHours * 7;
+};
+console.log(getIdealSleepHours());
+
+const calculateSleepDebt = () => {
+  const actualSleepHours = getActualSleepHours();
+  const idealSleepHours = getIdealSleepHours();
+  if (actualSleepHours === idealSleepHours) {
+    console.log("The user got the perfect amount of sleep");
+  } else if (actualSleepHours > idealSleepHours) {
+    console.log(`The user got more sleep than needed, ${actualSleepHours - idealSleepHours} more`);
+  } else if (actualSleepHours < idealSleepHours) {
+    console.log(`The user should get some rest and sleep ${idealSleepHours - actualSleepHours}`);
+  }
+};
+calculateSleepDebt();
