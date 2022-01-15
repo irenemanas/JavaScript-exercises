4ae0bad82332b138051b89c35506c9e3a5981dd8
irenemanas <irenemanas@gmail.com>
2022-01-15 19:36:57 +0100

    Add Story exercise

--- rockPaperScissors.js
+++ rockPaperScissors.js
@@ -256,3 +256,52 @@ for (let i = 0; i < input.length; i++) {
 }
 console.log(resultArray); // ['a', 'a', 'e', 'e', 'o', 'a', 'e', 'e', 'a']
 console.log(resultArray.join("").toUpperCase()); // AAEEOAEEA
+
+///////
+let story =
+  'Last weekend, I took literally the most beautiful bike ride of my life. The route is called "The 9W to Nyack" and it actually stretches all the way from Riverside Park in Manhattan to South Nyack, New Jersey. It\'s really an adventure from beginning to end! It is a 48 mile loop and it basically took me an entire day. I stopped at Riverbank State Park to take some extremely artsy photos. It was a short stop, though, because I had a really long way left to go. After a quick photo op at the very popular Little Red Lighthouse, I began my trek across the George Washington Bridge into New Jersey.  The GW is actually very long - 4,760 feet! I was already very tired by the time I got to the other side.  An hour later, I reached Greenbrook Nature Sanctuary, an extremely beautiful park along the coast of the Hudson.  Something that was very surprising to me was that near the end of the route you actually cross back into New York! At this point, you are very close to the end.';
+
+let overusedWords = ["really", "very", "basically"];
+
+let unnecessaryWords = ["extremely", "literally", "actually"];
+
+let storyWords = story.split(" ");
+//console.log(storyWords);
+console.log(storyWords.length); //188
+
+let betterWords = storyWords.filter((word) => !unnecessaryWords.includes(word));
+console.log(betterWords.length); //182
+
+for (let i = 0; i < storyWords.length; i++) {
+  if (storyWords[i] === overusedWords[i]) {
+    console.log(storyWords.length - overusedWords.length);
+  }
+}
+
+let totalTimes = 0;
+for (let word = 0; word < betterWords.length; word++) {
+  if (betterWords[word] === "really") {
+    totalTimes = totalTimes + 1;
+  } else if (betterWords[word] === "very") {
+    totalTimes = totalTimes + 1;
+  } else if (betterWords[word] === "basically") {
+    totalTimes = totalTimes + 1;
+  }
+}
+console.log(totalTimes);
+
+let sentences = 0;
+let array = story.split(" ");
+//console.log(array);
+array.forEach((word) => {
+  if (word[word.length - 1] === "." || word[word.length - 1] === "!") {
+    sentences += 1;
+  }
+});
+console.log(array.length); //188
+console.log(sentences); //12
+
+///
+const randomNums = [1, 123, 25, 90, 3543, 42];
+const foundElement = randomNums.findIndex((num) => num > 200);
+console.log(foundElement); // 4
